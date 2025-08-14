2fed941f99e5b3facc58197ab46661bd1f3df9ca
1056493483 <1056493483@qq.com>
2025-08-14 16:30:24 +0800

    Add dummy MAC layer data

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
@@ -1075,11 +1075,11 @@ static void schedule_dummy_pdsch(module_id_t module_id,
         pdsch_pdu->mcsTable[0] = 0;
         pdsch_pdu->rvIndex[0] = 0;
         pdsch_pdu->TBSize[0] = dummy_tb_size;
-        pdsch_pdu->ndi[0] = 1; // New data indicator
-        pdsch_pdu->dataScramblingId = gNB_mac->physCellId;
+        pdsch_pdu->newDataIndicator[0] = 1; // New data indicator
+        pdsch_pdu->dataScramblingId = scc->physCellId;
         pdsch_pdu->numDmrsCdmGrpsNoData = 1;
         pdsch_pdu->dmrsConfigType = 0; // Type 1
-        pdsch_pdu->dlDmrsScramblingId = gNB_mac->physCellId;
+        pdsch_pdu->dlDmrsScramblingId = scc->physCellId;
         
         // Set DMRS and other parameters
         pdsch_pdu->dmrsConfigType = 0;
